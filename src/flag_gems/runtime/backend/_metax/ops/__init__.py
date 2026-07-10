5e6f26e35e87db4e049e0fe5aeb87f3e28097f22
Tiannn967 <131343057+Tiannn967@users.noreply.github.com>
2026-07-10 09:59:53 +0800

    Add MetaX mul implementation (#4630)
    * add metax mul implementation
    
    * support metax mul runtime metadata fallback before triton 3.3

--- src/flag_gems/runtime/backend/_metax/ops/__init__.py
+++ src/flag_gems/runtime/backend/_metax/ops/__init__.py
@@ -33,6 +33,7 @@ from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 from .min import min, min_dim
 from .mm import mm, mm_out
+from .mul import mul, mul_
 from .nonzero import nonzero
 from .ones import ones
 from .ones_like import ones_like
@@ -79,6 +80,8 @@ __all__ = [
     "min",
     "mm",
     "mm_out",
+    "mul",
+    "mul_",
     "nonzero",
     "ones",
     "ones_like",
