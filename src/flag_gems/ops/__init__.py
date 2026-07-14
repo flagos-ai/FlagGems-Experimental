a04cb8cd538637a93bbc4e90f01a4b9ff0421443
chx7514 <hxchen7514@gmail.com>
2026-07-14 20:03:09 +0800

    [KernelGen][Nvidia] Add split_with_sizes operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -483,6 +483,7 @@ from flag_gems.ops.special_shifted_chebyshev_polynomial_u import (
     special_shifted_chebyshev_polynomial_u_,
 )
 from flag_gems.ops.special_softmax import special_softmax
+from flag_gems.ops.split_with_sizes import split_with_sizes
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
@@ -1125,6 +1126,7 @@ __all__ = [
     "special_shifted_chebyshev_polynomial_u",
     "special_shifted_chebyshev_polynomial_u_",
     "special_softmax",
+    "split_with_sizes",
     "split_with_sizes_copy",
     "sqrt",
     "sqrt_",
