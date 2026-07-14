5dca3ec10a75542a793fb5d288c82ca8cae2ff35
bwbwzzz <957058626@qq.com>
2026-07-14 10:15:54 +0000

    [KernelGen][Nvidia] Add special_i0 operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -852,6 +852,8 @@ _FULL_CONFIG = (
     ("special_gammaln", special_gammaln),
     ("special_gammaln.out", special_gammaln_out),
     ("special_hermite_polynomial_h", special_hermite_polynomial_h),
+    ("special_i0", special_i0),
+    ("special_i0.out", special_i0_out),
     ("special_i0e", special_i0e),
     ("special_i0e_out", special_i0e_out),
     ("special_i1", special_i1),
