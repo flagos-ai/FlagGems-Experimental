ff518cd98f215a37fbd5de45a96bcd4c0696184f
chx7514 <hxchen7514@gmail.com>
2026-07-14 20:03:09 +0800

    [KernelGen][Nvidia] Add split_with_sizes operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -680,6 +680,7 @@ from flag_gems.ops.special_spherical_bessel_j0 import (
     special_spherical_bessel_j0_,
 )
 from flag_gems.ops.special_xlog1py import special_xlog1py
+from flag_gems.ops.split_with_sizes import split_with_sizes
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
@@ -1527,6 +1528,7 @@ __all__ = [
     "special_spherical_bessel_j0",
     "special_spherical_bessel_j0_",
     "special_softmax",
+    "split_with_sizes",
     "split_with_sizes_copy",
     "sqrt",
     "sqrt_",
