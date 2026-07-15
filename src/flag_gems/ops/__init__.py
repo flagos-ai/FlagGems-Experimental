fb574571c99af5dd708a2bec36603740d175527e
bwbwzzz <957058626@qq.com>
2026-07-15 13:03:19 +0800

    [KernelGen][Nvidia] Add special_softmax operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -479,6 +479,7 @@ from flag_gems.ops.special_shifted_chebyshev_polynomial_u import (
     special_shifted_chebyshev_polynomial_u,
     special_shifted_chebyshev_polynomial_u_,
 )
+from flag_gems.ops.special_softmax import special_softmax
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
@@ -1116,6 +1117,7 @@ __all__ = [
     "special_log_softmax",
     "special_shifted_chebyshev_polynomial_u",
     "special_shifted_chebyshev_polynomial_u_",
+    "special_softmax",
     "split_with_sizes_copy",
     "sqrt",
     "sqrt_",
