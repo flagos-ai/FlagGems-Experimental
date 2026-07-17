321442450fb446bee9ca986004fe2e31077cf17f
chx7514 <hxchen7514@gmail.com>
2026-07-17 14:34:36 +0800

    [KernelGen][thead] Add index_copy_ vendor specialization

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -1,5 +1,8 @@
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
+from .index_copy_ import index_copy, index_copy_
 
 __all__ = [
     "adaptive_max_pool3d_backward",
+    "index_copy",
+    "index_copy_",
 ]
