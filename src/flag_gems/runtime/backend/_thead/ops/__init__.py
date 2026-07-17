b30899c5719af480ac43077fce72d55f1c284b0d
chx7514 <hxchen7514@gmail.com>
2026-07-17 13:22:37 +0800

    [KernelGen][thead] Add broadcast_to vendor specialization

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -1,5 +1,7 @@
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
+from .broadcast_to import broadcast_to
 
 __all__ = [
     "adaptive_max_pool3d_backward",
+    "broadcast_to",
 ]
