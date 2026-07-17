ae6b5be924fa401cecf4b12a18e3a0336caf15c8
chx7514 <hxchen7514@gmail.com>
2026-07-17 14:22:08 +0800

    [KernelGen][thead] Add log_normal_ vendor specialization

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -13,8 +13,10 @@
 # limitations under the License.
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .broadcast_to import broadcast_to
+from .log_normal_ import log_normal_
 
 __all__ = [
     "adaptive_max_pool3d_backward",
     "broadcast_to",
+    "log_normal_",
 ]
