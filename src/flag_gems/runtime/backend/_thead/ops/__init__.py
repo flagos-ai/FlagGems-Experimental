4fe901968be8817893032b497fb0efb6184dcdd2
chx7514 <hxchen7514@gmail.com>
2026-07-20 10:57:43 +0800

    [KernelGen][thead] Add embedding_dense_backward vendor specialization

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -14,12 +14,14 @@
 
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .broadcast_to import broadcast_to
+from .embedding_dense_backward import embedding_dense_backward
 from .index_copy_ import index_copy, index_copy_
 from .log_normal_ import log_normal_
 
 __all__ = [
     "adaptive_max_pool3d_backward",
     "broadcast_to",
+    "embedding_dense_backward",
     "index_copy",
     "index_copy_",
     "log_normal_",
