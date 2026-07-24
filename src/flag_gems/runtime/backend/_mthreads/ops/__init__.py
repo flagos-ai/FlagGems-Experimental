67a5c0f75644c15e67a6fc2d28b464ab9e3e01cd
1572161937 <1572161937@qq.com>
2026-07-24 18:00:12 +0800

    [KernelGen][MThreads] Add renorm_ Moore Threads specialized operator

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -48,6 +48,7 @@ from .rand_like import rand_like
 from .randn import randn
 from .randn_like import randn_like
 from .randperm import randperm
+from .renorm_ import renorm_
 from .repeat import repeat
 from .repeat_interleave import (
     repeat_interleave_self_int,
@@ -112,6 +113,7 @@ __all__ = [
     "randn",
     "randn_like",
     "randperm",
+    "renorm_",
     "repeat",
     "repeat_interleave_self_int",
     "repeat_interleave_self_tensor",
