02e6075972fcff07af361c52580b158f9da183d4
1572161937 <1572161937@qq.com>
2026-07-25 22:07:37 +0800

    [KernelGen][MThreads] Fix reflection_pad3d_backward logger name to backend-specific convention

--- src/flag_gems/runtime/backend/_mthreads/ops/reflection_pad3d_backward.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/reflection_pad3d_backward.py
@@ -26,7 +26,9 @@ from flag_gems.runtime import torch_device_fn
 from flag_gems.utils import libentry
 from flag_gems.utils import triton_lang_extension as ext
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(
+    f'flag_gems.runtime.backend._mthreads.ops.{__name__.split(".")[-1]}'
+)
 
 _SUPPORTED_DTYPES = {torch.float16, torch.bfloat16, torch.float32}
 
