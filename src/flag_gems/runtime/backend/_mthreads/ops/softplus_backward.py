9fd5d74f5e6cfd97128ec1c3b8292b2682aaa3b4
1572161937 <1572161937@qq.com>
2026-07-25 22:07:28 +0800

    [KernelGen][MThreads] Fix softplus_backward logger name to backend-specific convention

--- src/flag_gems/runtime/backend/_mthreads/ops/softplus_backward.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/softplus_backward.py
@@ -26,7 +26,9 @@ from flag_gems.ops.softplus import (
 from flag_gems.runtime import torch_device_fn
 from flag_gems.utils import libentry, tl_extra_shim
 
-logger = logging.getLogger(__name__)
+logger = logging.getLogger(
+    f'flag_gems.runtime.backend._mthreads.ops.{__name__.split(".")[-1]}'
+)
 
 _SUPPORTED_DTYPES = {torch.float16, torch.bfloat16, torch.float32}
 exp = tl_extra_shim.exp
