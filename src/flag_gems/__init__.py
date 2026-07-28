1e3410660c3816d593a7a4f6575275f1d652f035
bwbwzzz <957058626@qq.com>
2026-07-28 13:01:03 +0800

    [KernelGen][Nvidia] Add hardshrink operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -109,6 +109,7 @@ _FULL_CONFIG = (
     ("_is_all_true", _is_all_true),
     ("_jagged_to_padded_dense_forward", _jagged_to_padded_dense_forward),
     ("_linalg_eigvals", _linalg_eigvals),
+    ("hardshrink", hardshrink),
     ("linalg_svdvals", linalg_svdvals),
     ("_log_softmax", log_softmax),
     ("_log_softmax.out", log_softmax_out),
