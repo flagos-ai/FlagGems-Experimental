97b0dd907669f314838341c192f588991f22b901
chx7514 <49188554+chx7514@users.noreply.github.com>
2026-07-29 10:09:04 +0800

    [KernelGen][thead] Add lcm vendor specialization (#142)

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -14,6 +14,7 @@
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .broadcast_to import broadcast_to
 from .index_copy_ import index_copy, index_copy_
+from .lcm import lcm, lcm_
 from .log_normal_ import log_normal_
 from .special_chebyshev_polynomial_u import special_chebyshev_polynomial_u
 
@@ -22,6 +23,8 @@ __all__ = [
     "broadcast_to",
     "index_copy",
     "index_copy_",
+    "lcm",
+    "lcm_",
     "log_normal_",
     "special_chebyshev_polynomial_u",
 ]
