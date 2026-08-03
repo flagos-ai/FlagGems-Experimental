fd0467cd1e2f508fa0ebb047199f53ff5a81f320
chx7514 <49188554+chx7514@users.noreply.github.com>
2026-08-03 15:48:46 +0800

    [KernelGen][thead] Add linalg_svdvals vendor specialization (#143)

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -15,6 +15,7 @@ from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .broadcast_to import broadcast_to
 from .index_copy_ import index_copy, index_copy_
 from .lcm import lcm, lcm_
+from .linalg_svdvals import linalg_svdvals
 from .log_normal_ import log_normal_
 from .special_chebyshev_polynomial_u import special_chebyshev_polynomial_u
 
@@ -25,6 +26,7 @@ __all__ = [
     "index_copy_",
     "lcm",
     "lcm_",
+    "linalg_svdvals",
     "log_normal_",
     "special_chebyshev_polynomial_u",
 ]
