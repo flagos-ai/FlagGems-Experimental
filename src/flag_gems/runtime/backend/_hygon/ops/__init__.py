ee2eee786c439f9e08a039a4be1cbef2da98511b
Xiaozhi Liu <165480625+xzliu-opt@users.noreply.github.com>
2026-08-04 21:11:54 +0800

    [KernelGen][Hygon] Add lcm Hygon specialized operator (#232)

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -51,6 +51,7 @@ from .hadamard_transform import hadamard_transform
 from .index_add import index_add, index_add_
 from .index_copy_ import index_copy, index_copy_
 from .isin import isin
+from .lcm import lcm, lcm_
 from .log_normal_ import log_normal_
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
@@ -109,6 +110,8 @@ __all__ = [
     "index_copy",
     "index_copy_",
     "isin",
+    "lcm",
+    "lcm_",
     "log_normal_",
     "matmul_bf16",
     "matmul_int8",
