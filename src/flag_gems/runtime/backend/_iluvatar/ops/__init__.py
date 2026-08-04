134aec6f360bea28b701f9b03e8ac14f2b271e53
yzw1128 <120315675+yzw1128@users.noreply.github.com>
2026-08-04 21:14:00 +0800

    [KernelGen][Iluvatar] Add softplus_backward Iluvatar specialized operator (#215)
    Co-authored-by: yzw1128 <yzw1128@users.noreply.github.com>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -26,6 +26,7 @@ from .mm import mm, mm_out
 from .nonzero_numpy import nonzero_numpy
 from .repeat import repeat
 from .scatter_add import scatter_add_
+from .softplus import softplus_backward
 from .special_modified_bessel_k1 import (
     special_modified_bessel_k1,
     special_modified_bessel_k1_out,
@@ -52,6 +53,7 @@ __all__ = [
     "matmul_int8",
     "repeat",
     "scatter_add_",
+    "softplus_backward",
     "special_modified_bessel_k1",
     "special_modified_bessel_k1_out",
     "special_shifted_chebyshev_polynomial_w",
