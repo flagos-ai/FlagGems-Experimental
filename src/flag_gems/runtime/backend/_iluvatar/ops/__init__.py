287a49f52a21c7875fe8bdf31e2b07e40a06072d
yzw1128 <120315675+yzw1128@users.noreply.github.com>
2026-08-04 21:21:31 +0800

    [KernelGen][Iluvatar] Add diagonal_scatter Iluvatar specialized operator (#219)
    Co-authored-by: yzw1128 <yzw1128@users.noreply.github.com>
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -16,6 +16,7 @@ from .addmm import addmm, addmm_, addmm_out
 from .avg_pool3d import avg_pool3d_backward
 from .conv_depthwise2d import _conv_depthwise2d
 from .conv_transpose1d import conv_transpose1d
+from .diagonal_scatter import diagonal_scatter
 from .div import div_mode, div_mode_
 from .hadamard_transform import hadamard_transform
 from .histc import histc
@@ -44,6 +45,7 @@ __all__ = [
     "addmm_",
     "addmm_out",
     "avg_pool3d_backward",
+    "diagonal_scatter",
     "div_mode",
     "div_mode_",
     "hadamard_transform",
