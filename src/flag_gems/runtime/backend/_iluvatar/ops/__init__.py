332cd8eb13ce1c706241983c5daf7911cb5bfb07
yzw1128 <yzw1128@users.noreply.github.com>
2026-08-10 17:04:48 +0800

    [KernelGen][Iluvatar] Add 6 Iluvatar specialized operators: adaptive_max_pool3d_backward, _linalg_eigvals, linear_backward, nextafter, _thnn_fused_lstm_cell_backward_impl, _unsafe_masked_index_put_accumulate

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -1,32 +1,48 @@
+from .adaptive_max_pool3d_backward import run as adaptive_max_pool3d_backward
 from .addmm import addmm, addmm_out
 from .conv_depthwise2d import _conv_depthwise2d
 from .conv_transpose1d import conv_transpose1d
 from .div import div_mode, div_mode_
 from .hadamard_transform import hadamard_transform
+from .linalg_eigvals import run as _linalg_eigvals
 from .linear import linear
+from .linear_backward import run as linear_backward
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 from .mm import mm, mm_out
+from .nextafter import run as nextafter
 from .repeat import repeat
+from .thnn_fused_lstm_cell_backward_impl import (
+    run as _thnn_fused_lstm_cell_backward_impl,
+)
 from .tile import tile
+from .unsafe_masked_index_put_accumulate import (
+    run as _unsafe_masked_index_put_accumulate,
+)
 from .var import var, var_correction, var_dim
 
 __all__ = [
     "_conv_depthwise2d",
-    "conv_transpose1d",
+    "_linalg_eigvals",
+    "_thnn_fused_lstm_cell_backward_impl",
+    "_unsafe_masked_index_put_accumulate",
+    "adaptive_max_pool3d_backward",
     "addmm",
     "addmm_out",
+    "conv_transpose1d",
     "div_mode",
     "div_mode_",
     "hadamard_transform",
     "linear",
+    "linear_backward",
     "matmul_bf16",
     "matmul_int8",
+    "nextafter",
+    "mm",
+    "mm_out",
     "repeat",
     "tile",
     "var",
     "var_correction",
     "var_dim",
-    "mm",
-    "mm_out",
 ]
