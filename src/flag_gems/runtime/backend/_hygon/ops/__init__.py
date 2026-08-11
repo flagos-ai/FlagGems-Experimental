a6d8a16424bbae93fe721e537976114697a4923c
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-08-11 16:18:38 +0800

    [KernelGen][Hygon] Add reflection_pad3d_backward Hygon specialized operator (#274)
    Co-authored-by: taooo <gumptao2997@gmail.com>

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -75,6 +75,7 @@ from .pow import (
     pow_tensor_tensor_,
 )
 from .randperm import randperm
+from .reflection_pad3d_backward import reflection_pad3d_backward
 from .renorm_ import renorm_
 from .repeat import repeat
 from .silu import silu, silu_, silu_backward
@@ -149,6 +150,7 @@ __all__ = [
     "pow_tensor_tensor",
     "pow_tensor_tensor_",
     "randperm",
+    "reflection_pad3d_backward",
     "remainder",
     "remainder_",
     "renorm_",
