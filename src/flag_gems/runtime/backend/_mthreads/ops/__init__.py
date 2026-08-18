4844157e4b18b22ec98f7b6088b6fc039df8af55
1572161937 <1572161937@qq.com>
2026-08-18 17:31:55 +0800

    [KernelGen][MThreads] Add mish_ Moore Threads specialized operator

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -58,6 +58,7 @@ from .max import max, max_dim
 from .median import median, median_dim, median_dim_values, median_out
 from .min import min, min_dim
 from .mish import mish, mish_
+from .mish_ import mish_
 from .mode import mode
 from .mul import mul, mul_
 from .nonzero_numpy import nonzero_numpy
