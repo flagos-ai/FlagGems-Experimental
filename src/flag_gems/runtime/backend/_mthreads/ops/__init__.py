7c2e685e8cdf28f1df54e7907fa64341a96a80e8
1572161937 <1572161937@qq.com>
2026-08-18 17:31:51 +0800

    [KernelGen][MThreads] Add fix Moore Threads specialized operator

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -35,6 +35,7 @@ from .div import (
 from .dropout import dropout, dropout_backward
 from .erfinv import erfinv
 from .erfinv_ import erfinv_
+from .fix import fix
 from .flip import flip
 from .fmod_ import fmod_, fmod_scalar_, fmod_tensor_
 from .gather import gather, gather_backward
@@ -115,6 +116,7 @@ __all__ = [
     "dropout_backward",
     "erfinv",
     "erfinv_",
+    "fix",
     "flip",
     "fmod_",
     "fmod_scalar_",
