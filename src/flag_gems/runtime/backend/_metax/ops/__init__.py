6116a06e4435e0c886781ed1e0e784d0cd86f110
tianxiao <tianxiao@baai.com>
2026-09-04 09:56:13 +0000

    feat(metax): add 40 retest-passing operators to _metax backend
    Operators added (all accuracy + performance verified):
    - adaptive_avg_pool2d_backward, addmv_, arccosh_, as_strided_scatter
    - cholesky_inverse, conv_depthwise2d, cudnn_convolution
    - erfinv, erfinv_, float_power_, gcd_, gt_scalar_, gt_tensor_
    - histc, index_select_backward, kthvalue
    - lcm, lcm_, lgamma_, linalg_cholesky, linalg_ldl_factor, linalg_svdvals
    - linear_backward, logcumsumexp, logcumsumexp_out, logical_not_
    - max_pool3d_with_indices_backward, mvlgamma, ormqr
    - replication_pad3d_backward, scalar_tensor
    - special_bessel_j0, special_bessel_y0, special_gammaln, special_gammaln_out
    - special_legendre_polynomial_p, special_multigammaln
    - special_round, special_round_out
    - unsafe_masked_index_put_accumulate, upsample_nearest_exact2d_backward
    - weight_int8pack_mm
    
    All operators meet PASS criteria:
    - Accuracy: pytest passes
    - Performance: speedup ≥ 0.8x vs torch baseline
    
    Test environment: MetaX C550, MACA 3.7.2.0

--- src/flag_gems/runtime/backend/_metax/ops/__init__.py
+++ src/flag_gems/runtime/backend/_metax/ops/__init__.py
@@ -1,40 +1,65 @@
 from ._make_dep_token import _make_dep_token
 from ._nested_view_from_buffer_copy import _nested_view_from_buffer_copy
 from ._thnn_fused_lstm_cell_backward_impl import run
+from .adaptive_avg_pool2d_backward import _adaptive_avg_pool2d_backward
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
 from .addmm import addmm
+from .addmv_ import addmv_
 from .alpha_dropout import alpha_dropout
 from .amax import amax
 from .arange import arange, arange_start
+from .arccosh_ import arccosh_
+from .as_strided_scatter import as_strided_scatter
 from .avg_pool3d import avg_pool3d_backward
 from .bmm import bmm
 from .broadcast_to import broadcast_to
+from .cholesky_inverse import cholesky_inverse
 from .cholesky_solve import cholesky_solve, cholesky_solve_out
+from .conv_depthwise2d import _conv_depthwise2d
 from .conv_transpose1d import conv_transpose1d, conv_transpose1d_output_size
+from .cudnn_convolution import cudnn_convolution
+from .erfinv import erfinv
+from .erfinv_ import erfinv_
 from .exponential_ import exponential_
+from .float_power_ import float_power_tensor_scalar_
 from .full import full
 from .full_like import full_like
+from .gcd_ import gcd_
 from .greater_equal import greater_equal_
 from .groupnorm import group_norm
+from .gt_scalar_ import gt_scalar_
+from .gt_tensor_ import gt_tensor_
 from .hadamard_transform import hadamard_transform
+from .histc import histc
 from .index import index
 from .index_put import index_put, index_put_
 from .index_select import index_select
+from .index_select_backward import index_select_backward
 from .isin import isin
 from .kthvalue import kthvalue
 from .layernorm import layer_norm, layer_norm_backward
+from .lcm import lcm
+from .lcm_ import lcm_
 from .lgamma_ import lgamma, lgamma_
+from .linalg_cholesky import linalg_cholesky
+from .linalg_ldl_factor import ldl_factor
 from .linalg_svdvals import linalg_svdvals
+from .linear_backward import linear_backward
 from .log_sigmoid_forward import log_sigmoid_forward
 from .log_softmax import log_softmax, log_softmax_backward
+from .logcumsumexp import logcumsumexp
+from .logcumsumexp_out import logcumsumexp_out
+from .logical_not_ import logical_not_
 from .logical_or import logical_or, logical_or_
 from .lt_ import lt_, lt_scalar_
 from .masked_fill import masked_fill, masked_fill_
 from .masked_scatter import masked_scatter, masked_scatter_, masked_scatter_impl
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
+from .max_pool3d_with_indices_backward import max_pool3d_with_indices_backward
 from .min import min, min_dim
 from .mm import mm, mm_out
+from .mvlgamma import mvlgamma
 from .mvlgamma_ import mvlgamma_
 from .nansum import nansum, nansum_out
 from .new_ones import new_ones
@@ -42,88 +67,126 @@ from .nonzero import nonzero
 from .nonzero_numpy import nonzero_numpy
 from .ones import ones
 from .ones_like import ones_like
+from .ormqr import ormqr
 from .outer import outer
 from .polar import polar
 from .prod import prod, prod_dim
 from .renorm import renorm, renorm_
 from .repeat import repeat
 from .repeat_interleave import repeat_interleave_self_tensor
+from .replication_pad3d_backward import replication_pad3d_backward
 from .resolve_conj import resolve_conj
 from .rsqrt import rsqrt, rsqrt_
+from .scalar_tensor import scalar_tensor
 from .sigmoid import sigmoid
 from .special_bessel_j0 import special_bessel_j0, special_bessel_j0_out
+from .special_bessel_y0 import special_bessel_y0
 from .special_chebyshev_polynomial_u import special_chebyshev_polynomial_u
 from .special_chebyshev_polynomial_w import (
     special_chebyshev_polynomial_w,
     special_chebyshev_polynomial_w_out,
 )
 from .special_gammainc import special_gammainc
+from .special_gammaln import special_gammaln
+from .special_gammaln_out import special_gammaln_out
+from .special_legendre_polynomial_p import special_legendre_polynomial_p
+from .special_multigammaln import special_multigammaln
+from .special_round import special_round
+from .special_round_out import special_round_out
 from .special_shifted_chebyshev_polynomial_w import (
     special_shifted_chebyshev_polynomial_w,
 )
 from .tanh import tanh
 from .to_copy import to_copy
 from .unique import _unique2
+from .unsafe_masked_index_put_accumulate import _unsafe_masked_index_put_accumulate
 from .upsample_linear1d import upsample_linear1d
 from .upsample_nearest2d import upsample_nearest2d
+from .upsample_nearest_exact2d_backward import _upsample_nearest_exact2d_backward
+from .weight_int8pack_mm import weight_int8pack_mm
 from .zero import zero, zero_, zero_out
 from .zeros import zeros
 from .zeros_like import zeros_like
 
 __all__ = [
+    "_adaptive_avg_pool2d_backward",
+    "_conv_depthwise2d",
     "_make_dep_token",
     "_nested_view_from_buffer_copy",
     "_unique2",
+    "_unsafe_masked_index_put_accumulate",
+    "_upsample_nearest_exact2d_backward",
     "adaptive_max_pool3d_backward",
     "addmm",
+    "addmv_",
     "alpha_dropout",
     "amax",
     "arange",
     "arange_start",
+    "arccosh_",
+    "as_strided_scatter",
     "avg_pool3d_backward",
     "bmm",
     "broadcast_to",
+    "cholesky_inverse",
     "cholesky_solve",
     "cholesky_solve_out",
     "conv_transpose1d",
     "conv_transpose1d_output_size",
+    "cudnn_convolution",
+    "erfinv",
+    "erfinv_",
     "exponential_",
+    "float_power_tensor_scalar_",
     "full",
     "full_like",
+    "gcd_",
     "greater_equal_",
     "group_norm",
+    "gt_scalar_",
+    "gt_tensor_",
     "hadamard_transform",
+    "histc",
     "index",
     "index_put",
     "index_put_",
     "index_select",
+    "index_select_backward",
     "isin",
     "kthvalue",
     "layer_norm",
     "layer_norm_backward",
+    "lcm",
+    "lcm_",
+    "ldl_factor",
     "lgamma",
     "lgamma_",
+    "linalg_cholesky",
     "linalg_svdvals",
+    "linear_backward",
     "log_sigmoid_forward",
-    "lgamma",
-    "lgamma_",
     "log_softmax",
     "log_softmax_backward",
+    "logcumsumexp",
+    "logcumsumexp_out",
+    "logical_not_",
     "logical_or",
     "logical_or_",
     "lt_",
     "lt_scalar_",
+    "masked_fill",
+    "masked_fill_",
     "masked_scatter",
     "masked_scatter_",
     "masked_scatter_impl",
     "matmul_bf16",
     "matmul_int8",
-    "masked_fill",
-    "masked_fill_",
-    "min_dim",
+    "max_pool3d_with_indices_backward",
     "min",
+    "min_dim",
     "mm",
     "mm_out",
+    "mvlgamma",
     "mvlgamma_",
     "nansum",
     "nansum_out",
@@ -132,6 +195,7 @@ __all__ = [
     "nonzero_numpy",
     "ones",
     "ones_like",
+    "ormqr",
     "outer",
     "polar",
     "prod",
@@ -140,22 +204,32 @@ __all__ = [
     "renorm_",
     "repeat",
     "repeat_interleave_self_tensor",
+    "replication_pad3d_backward",
     "resolve_conj",
     "rsqrt",
     "rsqrt_",
     "run",
+    "scalar_tensor",
     "sigmoid",
     "special_bessel_j0",
     "special_bessel_j0_out",
+    "special_bessel_y0",
     "special_chebyshev_polynomial_u",
     "special_chebyshev_polynomial_w",
     "special_chebyshev_polynomial_w_out",
     "special_gammainc",
+    "special_gammaln",
+    "special_gammaln_out",
+    "special_legendre_polynomial_p",
+    "special_multigammaln",
+    "special_round",
+    "special_round_out",
     "special_shifted_chebyshev_polynomial_w",
     "tanh",
     "to_copy",
     "upsample_linear1d",
     "upsample_nearest2d",
+    "weight_int8pack_mm",
     "zero",
     "zero_",
     "zero_out",
