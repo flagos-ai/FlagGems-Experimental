e9b632cfb7e301a9827087a00a92beb9b6fe662a
xuejing.li <xjli@baai.ac.cn>
2026-09-07 17:51:58 +0800

    [KernelGen][Iluvatar] Add acos_ specialized operator
    Signed-off-by: xuejing.li <xjli@baai.ac.cn>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -16,6 +16,7 @@ import importlib
 
 from ..utils.pointwise_dynamic import ModuleGenerator
 from ._native_batch_norm_legit_functional import _native_batch_norm_legit_functional
+from .acos_ import acos_
 from .adaptive_max_pool3d_backward import run
 from .addmm import addmm, addmm_out
 from .addmm_ import addmm_
@@ -80,6 +81,7 @@ _pointwise_dynamic.ModuleGenerator = ModuleGenerator
 __all__ = [
     "_conv_depthwise2d",
     "_native_batch_norm_legit_functional",
+    "acos_",
     "addmm",
     "addmm_",
     "addmm_out",
@@ -115,8 +117,8 @@ __all__ = [
     "permute_copy",
     "renorm_",
     "repeat",
-    "resolve_neg",
     "repeat_interleave_self_int",
+    "resolve_neg",
     "run",
     "scatter_add_",
     "softplus_backward",
