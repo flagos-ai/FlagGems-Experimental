525fedda08b3c65cbb4dc4180e030a752b5e9799
lukatao <60033270+lukatao@users.noreply.github.com>
2026-09-15 11:40:11 +0800

    Add Kunlunxin specialization for erfc

--- src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
@@ -126,6 +126,7 @@ from .elu import elu, elu_, elu_backward
 from .embedding import embedding, embedding_backward
 from .eq import eq, eq_scalar
 from .erf import erf, erf_
+from .erfc import erfc
 from .exp import exp, exp_, exp_out
 from .exp2 import exp2, exp2_
 from .expm1 import expm1, expm1_, expm1_out
@@ -783,4 +784,5 @@ __all__ = [
     "zero_out",
     "zeros",
     "zeros_like",
+    "erfc",
 ]
