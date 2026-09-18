db13903f05be5fdaccc51beb47c7fd635a50ddf8
lukatao <60033270+lukatao@users.noreply.github.com>
2026-09-18 16:13:09 +0800

    Apply formatting to merged backend module

--- src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/__init__.py
@@ -39,8 +39,7 @@ from .apply_repetition_penalties import apply_repetition_penalties
 from .arange import arange, arange_start
 from .arccos import arccos, arccos_
 from .arcsin import arcsin, arcsin_, arcsin_out
-from .arctan import arctan_
-from .arctan import arctan
+from .arctan import arctan, arctan_
 from .argmax import argmax
 from .argmin import argmin
 from .as_strided_copy import as_strided_copy, as_strided_copy_out
